586294e755372c0b59300abf0fcaee20ae957cf6
Edgar Guardado <guardadoedgar20@gmail.com>
2023-07-29 18:54:16 -0700

    Add grid and color scheme

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ for (let i = 0; i < 16 * 16; i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("grid-item");
     gridContainer.appendChild(gridItem);
+
+    gridItem.addEventListener("mouseover", function () {
+        gridItem.style.backgroundColor = '#004C54';
+        gridItem.style.color = '#000000';
+        gridItem.style.border = '2px solid #000000';
+    })
+
+     gridItem.addEventListener('mouseleave', function () {
+        gridItem.style.backgroundColor = '';
+        gridItem.style.color = '';
+        gridItem.style.border = '';
+    })
 }
