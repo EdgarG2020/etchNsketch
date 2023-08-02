0ed9b4734ffebffbab72cc5b7f0fc7f74eb76bf8
Edgar Guardado <guardadoedgar20@gmail.com>
2023-08-02 14:41:38 -0700

    Add reset button

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-const gridContainer = document.querySelector(".grid-container")
+const gridContainer = document.querySelector('.grid-container')
+const resetBtn = document.querySelector('.reset-btn')
 
-for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridContainer.appendChild(gridItem);
+function createGrid() {
+    gridContainer.innerHTML = ''
+    for (let i = 0; i < 16 * 16; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
 
-    gridItem.addEventListener("mouseover", function () {
-        gridItem.style.backgroundColor = '#004C54';
-        gridItem.style.color = '#000000';
-        gridItem.style.border = '2px solid #000000';
-    })
+        gridItem.addEventListener('mouseover', function () {
+            gridItem.style.backgroundColor = '#004C54';
+            gridItem.style.color = '#000000';
+            gridItem.style.border = '2px solid #000000';
+        })
 
-     gridItem.addEventListener('mouseleave', function () {
-        gridItem.style.backgroundColor = '';
-        gridItem.style.color = '';
-        gridItem.style.border = '';
-    })
+        gridItem.addEventListener('mouseleave', function () {
+            gridItem.style.backgroundColor = '#004C54';
+            gridItem.style.color = '';
+            gridItem.style.border = '2px solid #000000';
+        })
+    }
 }
+
+createGrid()
+
+
+resetBtn.addEventListener('click', function () {
+    createGrid()
+})
